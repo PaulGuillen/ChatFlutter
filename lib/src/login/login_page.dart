5cf288f5907def740544a4adaa2ada605333d067
paulguillen190 <paulguillen190@gmail.com>
2022-05-19 19:52:02 -0500

    Implementing first route

---
+++ lib/src/login/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class  LoginPage extends StatefulWidget {
+  const  LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State< LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State< LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('HOLA MUNDO'),
+      ),
+    );
+  }
+}
+
+
