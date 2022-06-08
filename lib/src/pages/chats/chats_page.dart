e75aabe88b31aa64a3da015290680abab70c2edc
paulguillen190 <paulguillen190@gmail.com>
2022-06-08 10:27:21 -0500

    Creating a bottom navigation nive in home_page.dart - six day

---
+++ lib/src/pages/chats/chats_page.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/material.dart';
+
+class ChatsPage extends StatelessWidget {
+  const ChatsPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Center(
+        child: Text('Chat page'),
+      ),
+    );
+  }
+
+}
--- lib/src/pages/home/home_page.dart
+++ lib/src/pages/home/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:chat_flutter/src/pages/chats//chats_page.dart';
 import 'package:chat_flutter/src/pages/home/home_controller.dart';
+import 'package:chat_flutter/src/pages/profile/profile_page.dart';
+import 'package:chat_flutter/src/pages/users/users_page.dart';
+import 'package:chat_flutter/src/utils/my_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -9,9 +13,64 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
-      ),
+      bottomNavigationBar: bottomNavigationBar(context),
+        body: Obx(() => IndexedStack(
+          index: con.tabIndex.value,
+          children: [
+            ChatsPage(),
+            UsersPage(),
+            ProfilePage()
+          ],
+        ))
     );
   }
+
+  Widget bottomNavigationBar(BuildContext context) {
+    return Obx(() => MediaQuery(
+        data: MediaQuery.of(context).copyWith(textScaleFactor: 1.0),
+        child: Container(
+          height: 54, // ALTURA DEL BNB
+          child: BottomNavigationBar(
+            showUnselectedLabels: true,
+            showSelectedLabels: true,
+            onTap: con.changeTabIndex,
+            currentIndex: con.tabIndex.value,
+            backgroundColor: MyColors.primaryColor,
+            unselectedItemColor: Colors.white.withOpacity(0.5),
+            selectedItemColor: Colors.white,
+            items: [
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.chat,
+                    size: 20,
+                  ),
+                  label: 'Chats',
+                  backgroundColor: MyColors.primaryColor
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person,
+                    size: 20,
+                  ),
+                  label: 'Usuarios',
+                  backgroundColor: MyColors.primaryColor
+              ),
+              BottomNavigationBarItem(
+                  icon: Icon(
+                    Icons.person_pin_rounded,
+                    size: 20,
+                  ),
+                  label: 'Perfil',
+                  backgroundColor: MyColors.primaryColor
+              )
+
+            ],
+          ),
+
+        )
+    )
+    );
+  }
+
+
 }
