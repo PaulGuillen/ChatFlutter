6d5a4d0bdc21727b0c515c9fd5d47698eb0c0187
paulguillen190 <paulguillen190@gmail.com>
2022-06-08 12:31:41 -0500

    Creating perfil view, getting data  - six day

---
+++ lib/src/pages/profile/profile_controller.dart
@@ -0,0 +1,14 @@
+import 'package:chat_flutter/src/models/users.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+class ProfileController extends GetxController {
+
+  User user = User.fromJson(GetStorage().read('user') ?? {});
+
+  void signOut() {
+    GetStorage().remove('user');
+    Get.offNamedUntil('/', (route) => false);
+  }
+
+}
--- lib/src/pages/profile/profile_page.dart
+++ lib/src/pages/profile/profile_page.dart
@@ -1,14 +1,75 @@
 
+import 'package:chat_flutter/src/pages/profile/profile_controller.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class ProfilePage extends StatelessWidget {
-  const ProfilePage({Key? key}) : super(key: key);
+  ProfilePage({Key? key}) : super(key: key);
+
+  ProfileController con = Get.put(ProfileController());
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-      child: Text('Profile page'),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => con.signOut(),
+        child: Icon(Icons.power_settings_new),
+        backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(
+            children: [
+              circleImageUser(),
+              SizedBox(height: 20),
+              userInfo(
+                  'Nombre del usuario',
+                  '${con.user.name ?? ''} ${con.user.lastname ?? ''}',
+                  Icons.person
+              ),
+              userInfo(
+                  'Email',
+                  con.user.email ?? '',
+                  Icons.email
+              ),
+              userInfo(
+                  'Telefono',
+                  con.user.phone ?? '',
+                  Icons.phone
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget userInfo(String title, String subtitle, IconData iconData) {
+    return Container(
+      margin: EdgeInsets.only(left: 30, right: 30),
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(subtitle),
+        leading: Icon(iconData),
+      ),
+    );
+  }
+
+  Widget circleImageUser() {
+    return Center(
+      child: Container(
+        margin: EdgeInsets.only(top: 30),
+        width: 200,
+        child: AspectRatio(
+          aspectRatio: 1,
+          child: ClipOval(
+            child: FadeInImage.assetNetwork(
+                fit: BoxFit.cover,
+                placeholder: 'assets/img/user_profile_2.png',
+                image: 'https://img.freepik.com/foto-gratis/retrato-joven-sonriente-gafas_171337-4842.jpg?w=2000'
+            ),
+          ),
+        ),
       ),
     );
   }
