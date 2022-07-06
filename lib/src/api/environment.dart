8fd0c22497377e920bc8f6092d1b8eb87baa5aa1
PaulGuillen <paulguillen190@gmail.com>
2022-07-06 09:13:35 -0500

    User list

--- lib/src/api/environment.dart
+++ lib/src/api/environment.dart
@@ -3,5 +3,5 @@ class Environment{
 
   static const String API_CHAT = "http://192.168.100.128:3000/";
   static const String API_CHAT_OLD = "192.168.100.128:3000";
-
+  static const String IMAGE_URL = "https://eitrawmaterials.eu/wp-content/uploads/2016/09/person-icon.png";
 }
--- lib/src/pages/users/users_page.dart
+++ lib/src/pages/users/users_page.dart
@@ -1,18 +1,66 @@
+import 'package:chat_flutter/src/api/environment.dart';
 import 'package:chat_flutter/src/pages/users/users_controller.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+
+import '../../models/users.dart';
+import '../../utils/my_colors.dart';
 
 class UsersPage extends StatelessWidget {
 
   UsersController con = Get.put(UsersController());
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Users Page'),
+      appBar: AppBar(
+        title: Text('Lista de usuarios'),
+        automaticallyImplyLeading: false,
+        backgroundColor: MyColors.primaryColor,
+      ),
+      body: SafeArea(
+        child: FutureBuilder(
+          future: con.getUsers(),
+          builder: (context, AsyncSnapshot<List<User>> snapshot) {
+            if (snapshot.hasData) {
+              if (snapshot.data?.isNotEmpty == true) {
+                return ListView.builder(
+                    itemCount: snapshot.data?.length ?? 0,
+                    itemBuilder: (_, index) {
+                      return cardUser(snapshot.data![index]);
+                    }
+                );
+              }
+              else {
+                return Container();
+              }
+            }
+            else {
+              return Container();
+            }
+          },
+        ),
       ),
     );
   }
+
+  Widget cardUser(User user) {
+    return ListTile(
+      onTap: () => con.goToChat(user),
+      title: Text(user.name ?? ''),
+      subtitle: Text(user.email ?? ''),
+      leading: AspectRatio(
+        aspectRatio: 1,
+        child: ClipOval(
+          child: FadeInImage.assetNetwork(
+              fit: BoxFit.cover,
+              placeholder: 'assets/img/user_profile_2.png',
+              image: user.image ?? Environment.IMAGE_URL
+          ),
+        ),
+      ),
+    );
+  }
+
 
 }
