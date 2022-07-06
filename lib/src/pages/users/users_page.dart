422aac113cf8268b8d8be995faa22a7b8a739aa8
PaulGuillen <paulguillen190@gmail.com>
2022-07-06 08:23:04 -0500

    Data

--- lib/src/pages/users/users_page.dart
+++ lib/src/pages/users/users_page.dart
@@ -10,7 +10,7 @@ class UsersPage extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Text('Users '),
+        child: Text('Users PLage '),
       ),
     );
   }
