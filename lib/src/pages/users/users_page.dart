2b4669dd62ec8f5fefabc2a9e37b373391547d32
PAUL <paulguillen190@gmail.com>
2022-07-06 08:11:35 -0500

    Getting list per user avoiding main user

--- lib/src/pages/users/users_page.dart
+++ lib/src/pages/users/users_page.dart
@@ -10,7 +10,7 @@ class UsersPage extends StatelessWidget {
   Widget build(BuildContext context){
     return Scaffold(
       body: Center(
-        child: Text('Users page'),
+        child: Text('Users '),
       ),
     );
   }
