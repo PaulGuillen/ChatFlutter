70848ff64fda6035c1e066d444eb7425156a6300
PaulGuillen <paulguillen190@gmail.com>
2022-07-08 16:13:41 -0500

    Checking if a user is connected or not, same in backend

--- lib/src/pages/home/home_controller.dart
+++ lib/src/pages/home/home_controller.dart
@@ -1,6 +1,8 @@
+import 'package:chat_flutter/src/api/environment.dart';
 import 'package:chat_flutter/src/models/users.dart';
 import 'package:get/get.dart';
 import 'package:get_storage/get_storage.dart';
+import 'package:socket_io_client/socket_io_client.dart';
 
 class HomeController extends GetxController {
 
@@ -8,9 +10,34 @@ class HomeController extends GetxController {
 
   var tabIndex = 0.obs;
 
+
+  Socket socket = io('${Environment.API_CHAT}chat', <String, dynamic> {
+    'transports': ['websocket'],
+    'autoConnect': false
+  });
+
   HomeController() {
     print('USUARIO DE SESION: ${user.toJson()}');
+    connectAndListen();
   }
+
+  void connectAndListen() {
+    if (user.id != null) {
+      socket.connect();
+      socket.onConnect((data) {
+        print('USUARIO CONECTADO A SOCKET IO');
+
+      });
+    }
+  }
+
+  @override
+  void onClose() {
+    // TODO: implement onClose
+    super.onClose();
+    socket.disconnect();
+  }
+
 
   void changeTabIndex(int index) {
     tabIndex.value = index;
